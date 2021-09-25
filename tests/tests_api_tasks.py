e1644a51d47a138904927d84813eacab34f51120
Pierre Beaujean <pierreb24@gmail.com>
2021-09-25 16:54:49 +0200

    add ko tests

--- tests/tests_api_tasks.py
+++ tests/tests_api_tasks.py
@@ -62,6 +62,12 @@ class CategoriesTestCase(FlaskTestCase):
         self.assertEqual(data['id'], self.category_1.id)
         self.assertEqual(data['name'], self.category_1.name)
 
+    def test_view_unknown_category_ok(self):
+        self.assertEqual(self.num_category, Category.query.count())
+
+        response = self.client.get(flask.url_for('api.category', id=-1))
+        self.assertEqual(response.status_code, 404)
+
     def test_modify_category_ok(self):
         self.assertEqual(self.num_category, Category.query.count())
 
@@ -79,6 +85,16 @@ class CategoriesTestCase(FlaskTestCase):
         c = Category.query.get(data['id'])
         self.assertEqual(c.name, name)
 
+    def test_modify_unknown_category_ko(self):
+        self.assertEqual(self.num_category, Category.query.count())
+
+        name = 'whatever'
+
+        response = self.client.put(flask.url_for('api.category', id=-1), json={
+            'name': name
+        })
+        self.assertEqual(response.status_code, 404)
+
     def test_delete_category_ok(self):
         self.assertEqual(self.num_category, Category.query.count())
 
@@ -87,6 +103,14 @@ class CategoriesTestCase(FlaskTestCase):
 
         self.assertEqual(self.num_category - 1, Category.query.count())
         self.assertIsNone(Category.query.get(self.category_1.id))
+
+    def test_delete_unknown_category_ko(self):
+        self.assertEqual(self.num_category, Category.query.count())
+
+        response = self.client.delete(flask.url_for('api.category', id=-1))
+        self.assertEqual(response.status_code, 404)
+
+        self.assertEqual(self.num_category, Category.query.count())
 
 
 class TasksTestCase(FlaskTestCase):
@@ -128,6 +152,7 @@ class TasksTestCase(FlaskTestCase):
             'name': name,
             'color': color
         })
+        self.assertEqual(response.status_code, 200)
         data = response.get_json()
 
         self.assertEqual(name, data['name'])
@@ -139,6 +164,20 @@ class TasksTestCase(FlaskTestCase):
         self.assertEqual(name, t.name)
         self.assertEqual(color, t.color)
         self.assertEqual(self.category_1.id, t.category_id)
+
+    def test_create_task_unknown_cat_ko(self):
+        self.assertEqual(self.num_task, Task.query.count())
+
+        name = 'whatever'
+        color = '#ff0000'
+
+        response = self.client.post(flask.url_for('api.category', id=-1), json={
+            'name': name,
+            'color': color
+        })
+        self.assertEqual(response.status_code, 404)
+
+        self.assertEqual(self.num_task, Task.query.count())
 
     def test_view_task_ok(self):
         self.assertEqual(self.num_task, Task.query.count())
@@ -153,7 +192,13 @@ class TasksTestCase(FlaskTestCase):
         self.assertEqual(data['color'], t.color)
         self.assertEqual(data['category'], t.category_id)
 
-    def test_modify_category_ok(self):
+    def test_view_unknown_task_ok(self):
+        self.assertEqual(self.num_task, Task.query.count())
+
+        response = self.client.get(flask.url_for('api.task', id=-1))
+        self.assertEqual(response.status_code, 404)
+
+    def test_modify_task_ok(self):
         self.assertEqual(self.num_task, Task.query.count())
 
         name = 'whatever'
@@ -177,6 +222,19 @@ class TasksTestCase(FlaskTestCase):
         self.assertEqual(color, t.color)
         self.assertEqual(self.category_2.id, t.category_id)
 
+    def test_modify_unknown_task_ko(self):
+        self.assertEqual(self.num_task, Task.query.count())
+
+        name = 'whatever'
+        color = '#000000'
+
+        response = self.client.put(flask.url_for('api.task', id=-1), json={
+            'name': name,
+            'color': color,
+            'category': self.category_2.id
+        })
+        self.assertEqual(response.status_code, 404)
+
     def test_delete_task_ok(self):
         self.assertEqual(self.num_task, Task.query.count())
 
@@ -185,3 +243,11 @@ class TasksTestCase(FlaskTestCase):
 
         self.assertEqual(self.num_task - 1, Task.query.count())
         self.assertIsNone(Task.query.get(self.task_1_1.id))
+
+    def test_delete_unknown_task_ko(self):
+        self.assertEqual(self.num_task, Task.query.count())
+
+        response = self.client.delete(flask.url_for('api.task', id=-1))
+        self.assertEqual(response.status_code, 404)
+
+        self.assertEqual(self.num_task, Task.query.count())
