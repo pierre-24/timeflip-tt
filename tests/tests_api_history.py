aeec170e5776e948226a0155d53c9570deba91c6
Pierre Beaujean <pierreb24@gmail.com>
2021-10-01 21:32:36 +0200

    add CRUD HistoryElement

--- tests/tests_api_history.py
+++ tests/tests_api_history.py
@@ -7,7 +7,7 @@ from typing import List
 
 import flask
 
-from timefliptt.blueprints.base_models import HistoryElement, TimeFlipDevice
+from timefliptt.blueprints.base_models import HistoryElement, TimeFlipDevice, Category, Task
 
 
 class HistoryElementTestCase(FlaskTestCase):
@@ -63,31 +63,46 @@ class HistoryTestCase(FlaskTestCase):
     def setUp(self):
         super().setUp()
 
+        self.category = Category.create('x')
+        self.db_session.add(self.category)
+
         self.device = TimeFlipDevice.create('00:00:00:00:00:00', '000000')
         self.db_session.add(self.device)
         self.db_session.commit()
 
+        self.task = Task.create('x', self.category, '#000000')
+        self.db_session.add(self.task)
+
         num_elements = 10
         start = datetime.now() - timedelta(seconds=num_elements)
+
+        self.elements = []
 
         for i in range(num_elements):
             end = start + timedelta(seconds=1)
             element = HistoryElement.create(start, end, random.randrange(0, 63), self.device)
             self.db_session.add(element)
+            self.elements.append(element)
             start = end
 
         self.db_session.commit()
 
         self.num_elements = HistoryElement.query.count()
 
-    def test_get_history_ok(self):
+    def test_get_history_elements_ok(self):
         self.assertEqual(self.num_elements, HistoryElement.query.count())
 
         def elmts(page: int, page_size: int = 5, expected_status: int = 200) -> List[dict]:
-            response = self.client.get(flask.url_for('api.histories') + '?page={}&page_size={}'.format(page, page_size))
+            response = self.client.get(
+                flask.url_for('api.history-els') + '?page={}&page_size={}'.format(page, page_size))
             self.assertEqual(response.status_code, expected_status)
             if response.status_code == 200:
-                return response.get_json()['history']
+                data = response.get_json()
+
+                self.assertEqual(data['current_page'], page)
+                self.assertEqual(data['page_size'], page_size)
+
+                return data['history']
             else:
                 return []
 
@@ -99,6 +114,9 @@ class HistoryTestCase(FlaskTestCase):
         self.assertEqual(e.original_facet, data['original_facet'])
         self.assertEqual(e.timeflip_device_id, data['timeflip_device'])
         self.assertEqual(e.comment, data['comment'])
+        self.assertEqual(e.task_id, data['task'])
+        self.assertEqual(e.start.isoformat(), data['start'])
+        self.assertEqual(e.end.isoformat(), data['end'])
 
         # test page size
         self.assertEqual(len(elmts(0, 5)), 5)
@@ -114,4 +132,89 @@ class HistoryTestCase(FlaskTestCase):
 
         # test outside bond (get 404)
         self.assertEqual(len(elmts(self.num_elements + 1, 1, expected_status=404)), 0)
-        self.assertEqual(len(elmts(-2, 1, expected_status=404)), 0)
+        self.assertEqual(len(elmts(-2, 1, expected_status=422)), 0)
+
+    def test_get_history_element_ok(self):
+        self.assertEqual(self.num_elements, HistoryElement.query.count())
+        element = self.elements[0]
+
+        response = self.client.get(flask.url_for('api.history-el', id=element.id))
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+
+        self.assertEqual(element.id, data['id'])
+        self.assertEqual(element.original_facet, data['original_facet'])
+        self.assertEqual(element.timeflip_device_id, data['timeflip_device'])
+        self.assertEqual(element.comment, data['comment'])
+        self.assertEqual(element.task_id, data['task'])
+        self.assertEqual(element.start.isoformat(), data['start'])
+        self.assertEqual(element.end.isoformat(), data['end'])
+
+    def test_get_unknown_history_element_ko(self):
+        self.assertEqual(self.num_elements, HistoryElement.query.count())
+
+        response = self.client.get(flask.url_for('api.history-el', id=self.num_elements + 1))
+        self.assertEqual(response.status_code, 404)
+
+    def test_modify_history_element_ok(self):
+        self.assertEqual(self.num_elements, HistoryElement.query.count())
+        element = self.elements[0]
+
+        comment = 'whatever'
+        task = self.task
+
+        start = datetime.now() - timedelta(seconds=100)
+        end = start + timedelta(seconds=1)
+
+        response = self.client.put(flask.url_for('api.history-el', id=element.id), json={
+            'comment': comment,
+            'task': task.id,
+            'start': start.isoformat(),
+            'end': end.isoformat()
+        })
+        self.assertEqual(response.status_code, 200)
+
+        data = response.get_json()
+        self.assertEqual(comment, data['comment'])
+        self.assertEqual(task.id, data['task']['id'])
+        self.assertEqual(start.isoformat(), data['start'])
+        self.assertEqual(end.isoformat(), data['end'])
+
+        e = HistoryElement.query.get(element.id)
+        self.assertEqual(comment, e.comment)
+        self.assertEqual(task.id, e.task_id)
+        self.assertEqual(start, e.start)
+        self.assertEqual(end, e.end)
+
+    def test_modify_unknown_history_element_ko(self):
+        self.assertEqual(self.num_elements, HistoryElement.query.count())
+
+        response = self.client.put(flask.url_for('api.history-el', id=self.num_elements + 1))
+        self.assertEqual(response.status_code, 404)
+
+    def test_modify_history_element_unknown_task_ko(self):
+        self.assertEqual(self.num_elements, HistoryElement.query.count())
+        element = self.elements[0]
+
+        response = self.client.put(flask.url_for('api.history-el', id=element.id), json={
+            'task': Task.query.count() + 1
+        })
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_history_element_ok(self):
+        self.assertEqual(self.num_elements, HistoryElement.query.count())
+        element = self.elements[0]
+
+        response = self.client.delete(flask.url_for('api.history-el', id=element.id))
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(self.num_elements - 1, HistoryElement.query.count())
+        self.assertIsNone(HistoryElement.query.get(element.id))
+
+    def test_delete_unknown_history_element_ko(self):
+        self.assertEqual(self.num_elements, HistoryElement.query.count())
+
+        response = self.client.delete(flask.url_for('api.history-el', id=self.num_elements + 1))
+        self.assertEqual(response.status_code, 404)
+
+        self.assertEqual(self.num_elements, HistoryElement.query.count())
--- timefliptt/blueprints/api/views/views_history.py
+++ timefliptt/blueprints/api/views/views_history.py
@@ -1,28 +1,35 @@
+import math
+
 import flask
 from flask import jsonify, Response
 from flask.views import MethodView
 
+from webargs import fields
+from marshmallow import Schema, validate, post_load
+
+from timefliptt.app import db
 from timefliptt.blueprints.api.views import blueprint
 from timefliptt.blueprints.api.schemas import HistoryElementSchema, Parser
-from timefliptt.blueprints.base_models import HistoryElement
+from timefliptt.blueprints.base_models import HistoryElement, Task
 
 
 parser = Parser()
 
 
-class HistoriesView(MethodView):
-
+class HistoryElementsView(MethodView):
     PAGE_SIZE = 5
 
-    def get(self) -> Response:
+    class PaginateSchema(Schema):
+        page = fields.Integer(validate=validate.Range(min=0))
+        page_size = fields.Integer(validate=validate.Range(min=0))
+
+    @parser.use_kwargs(PaginateSchema, location='query')
+    def get(self, **kwargs) -> Response:
         """Get the list of history elements
         """
 
-        try:
-            page = int(flask.request.args.get('page', 0))
-            page_size = int(flask.request.args.get('page_size', self.PAGE_SIZE))
-        except ValueError:
-            flask.abort(422)
+        page = kwargs.get('page', 0)
+        page_size = kwargs.get('page_size', self.PAGE_SIZE)
 
         num_results = HistoryElement.query.count()
 
@@ -39,17 +46,77 @@ class HistoriesView(MethodView):
 
         previous_page = None
         if page > 0:
-            previous_page = FORMAT.format(flask.url_for('api.histories'), page - 1, page_size)
+            previous_page = FORMAT.format(flask.url_for('api.history-els'), page - 1, page_size)
         next_page = None
         if (page + 1) * page_size < num_results:
-            next_page = FORMAT.format(flask.url_for('api.histories'), page + 1, page_size)
+            next_page = FORMAT.format(flask.url_for('api.history-els'), page + 1, page_size)
 
         return jsonify(
-            total=num_results,
+            total_elements=num_results,
+            total_pages=int(math.ceil(num_results / page_size)),
+            current_page=page,
+            page_size=page_size,
             previous_page=previous_page,
             next_page=next_page,
             history=HistoryElementSchema(many=True).dump(results)
         )
 
 
-blueprint.add_url_rule('/api/history/', view_func=HistoriesView.as_view('histories'))
+blueprint.add_url_rule('/api/history/', view_func=HistoryElementsView.as_view('history-els'))
+
+
+class HistoryElementView(MethodView):
+
+    class SimpleHistoryElementSchema(Schema):
+        id = fields.Integer(validate=validate.Range(min=0))
+
+        @post_load
+        def make_object(self, data, **kwargs):
+            return HistoryElement.query.get(data['id'])
+
+    @parser.use_args(SimpleHistoryElementSchema, location='view_args')
+    def get(self, element: HistoryElement, id: int) -> Response:
+        if element is not None:
+            return jsonify(HistoryElementSchema().dump(element))
+        else:
+            flask.abort(404)
+
+    class ModifyHistorySchema(Schema):
+        task = fields.Integer(validate=validate.Range(min=0))
+        comment = fields.Str()
+        start = fields.DateTime()
+        end = fields.DateTime()
+
+    @parser.use_args(SimpleHistoryElementSchema, location='view_args')
+    @parser.use_kwargs(ModifyHistorySchema, location='json')
+    def put(self, element: HistoryElement, id: int, **kwargs) -> Response:
+        if element is not None:
+            if 'task' in kwargs:
+                task = Task.query.get(kwargs.get('task'))
+                if task is None:
+                    flask.abort(404)
+
+                element.task_id = task.id
+
+            els = ['comment', 'start', 'end']
+            for el in els:
+                if el in kwargs:
+                    setattr(element, el, kwargs.get(el))
+
+            db.session.add(element)
+            db.session.commit()
+            return jsonify(HistoryElementSchema().dump(element))
+        else:
+            flask.abort(404)
+
+    @parser.use_args(SimpleHistoryElementSchema, location='view_args')
+    def delete(self, element: HistoryElement, id: int) -> Response:
+        if element is not None:
+            db.session.delete(element)
+            db.session.commit()
+            return jsonify(status='ok')
+        else:
+            flask.abort(404)
+
+
+blueprint.add_url_rule('/api/history/<int:id>/', view_func=HistoryElementView.as_view('history-el'))
