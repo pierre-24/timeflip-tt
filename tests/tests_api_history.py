0a76b673e56c76bf2e0f9861390e84d524c25d06
Pierre Beaujean <pierreb24@gmail.com>
2021-10-16 19:57:21 +0200

    test common mixin

--- tests/tests_api_history.py
+++ tests/tests_api_history.py
@@ -1,13 +1,16 @@
 import random
 import math
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from tests import FlaskTestCase
 
 from typing import List
 
 import flask
+from flask.views import MethodView
 
 from timefliptt.blueprints.base_models import HistoryElement, TimeFlipDevice, Category, Task
+from timefliptt.blueprints.api.views.views_history import HistoryElementMixin
+from timefliptt.blueprints.api.schemas import Parser, HistoryElementSchema
 
 
 class HistoryElementTestCase(FlaskTestCase):
@@ -57,6 +60,258 @@ class HistoryElementTestCase(FlaskTestCase):
         # -----|###|--->
         #  s e
         self.assertEqual(0, h(base + _1sec, base + _1min).duration(end=base))
+
+
+class HistoryElementMixinCase(FlaskTestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        class FakeView(HistoryElementMixin, MethodView):
+
+            parser = Parser()
+
+            @parser.use_kwargs(HistoryElementMixin.FilterHistoryElementSchema, location='query')
+            def get(self, **kwargs):
+                """Get the list of history elements
+                """
+
+                elements, (start, end) = self.query_elements(**kwargs)
+
+                return flask.jsonify(
+                    elements=HistoryElementSchema(many=True).dump(elements.all()),
+                    start=start.isoformat(),
+                    end=end.isoformat()
+                )
+
+        self.app.add_url_rule('/api/whatever/the/fuck/', view_func=FakeView.as_view('test'))
+
+        self.device = TimeFlipDevice.create('00:00:00:00:00:00', '000000')
+        self.db_session.add(self.device)
+
+        self.num_categories = 3
+        self.categories = []
+        for i in range(self.num_categories):
+            category = Category.create('cat{}'.format(i))
+            self.db_session.add(category)
+            self.categories.append(category)
+
+        self.db_session.commit()
+
+        self.num_tasks = 10
+        self.tasks = []
+        for i in range(self.num_tasks):
+            task = Task.create('x{}'.format(i), self.categories[random.randrange(self.num_categories)], '#000000')
+            self.db_session.add(task)
+            self.tasks.append(task)
+
+        self.db_session.commit()
+
+        num_elements = 20
+        self.elements = []
+        self.start = datetime.now() - timedelta(minutes=num_elements)
+        start = self.start
+        delta = timedelta(days=1)
+        for i in range(num_elements):
+            end = start + delta
+            task = self.tasks[random.randrange(self.num_tasks)]
+
+            element = HistoryElement.create(start, end, random.randrange(0, 63), self.device, task)
+            self.db_session.add(element)
+            self.elements.append(element)
+            start = end
+
+        self.db_session.commit()
+        self.num_elements = HistoryElement.query.count()
+
+    def test_filter_start_end_date_ok(self):
+
+        def test(start: date = date.min, end: date = date.max):
+            elements, (s, e) = HistoryElementMixin.query_elements(start_date=start, end_date=end)
+
+            self.assertEqual(s.date(), start)
+            self.assertEqual(e.date(), end)
+
+            elements = elements.all()
+            actual_elements = list(filter(lambda e: e.start.date() < end and e.end.date() >= start, self.elements))
+
+            self.assertEqual(len(elements), len(actual_elements))
+            for element in actual_elements:
+                self.assertIn(element, elements)
+
+            response = self.client.get(
+                flask.url_for('test') + '?start_date={}&end_date={}'.format(start.isoformat(), end.isoformat()))
+            self.assertEqual(response.status_code, 200)
+            data = response.get_json()
+
+            actual_elements_id = [e.id for e in actual_elements]
+            for i in map(lambda e: e['id'], data['elements']):
+                self.assertIn(i, actual_elements_id)
+
+        # test all element
+        test()
+
+        # test with no element
+        test(end=self.start.date())
+
+        # test with some elements
+        test(start=self.elements[self.num_elements - 10].start.date())
+        test(end=self.elements[self.num_elements - 10].end.date())
+
+    def test_filter_start_end_ok(self):
+
+        def test(start: datetime = datetime.min, end: datetime = datetime.max):
+            elements, (s, e) = HistoryElementMixin.query_elements(start=start, end=end)
+
+            self.assertEqual(s, start)
+            self.assertEqual(e, end)
+
+            elements = elements.all()
+            actual_elements = list(filter(lambda e: e.start < end and e.end > start, self.elements))
+
+            self.assertEqual(len(elements), len(actual_elements))
+            for element in actual_elements:
+                self.assertIn(element, elements)
+
+            response = self.client.get(
+                flask.url_for('test') + '?start={}&end={}'.format(start.isoformat(), end.isoformat()))
+            self.assertEqual(response.status_code, 200)
+            data = response.get_json()
+
+            actual_elements_id = [e.id for e in actual_elements]
+            for i in map(lambda e: e['id'], data['elements']):
+                self.assertIn(i, actual_elements_id)
+
+        # test all element
+        test()
+
+        # test with no element
+        test(end=self.start)
+
+        # test with some elements
+        test(start=self.elements[self.num_elements - 10].start)
+        test(end=self.elements[self.num_elements - 10].end)
+
+    def test_filter_tasks_ok(self):
+        def test(tasks: List[int] = None):
+            actual_elements = self.elements
+
+            kwargs = {}
+            if tasks is not None:
+                kwargs['task'] = tasks
+                actual_elements = list(filter(lambda e: e.task_id in tasks, self.elements))
+
+            elements, _ = HistoryElementMixin.query_elements(**kwargs)
+            elements = elements.all()
+
+            self.assertEqual(len(elements), len(actual_elements))
+            for element in actual_elements:
+                self.assertIn(element, elements)
+
+            url = flask.url_for('test')
+            if tasks is not None and len(tasks) > 0:
+                url += '?task={}'.format('&task='.join(str(t) for t in tasks))
+
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            data = response.get_json()
+
+            actual_elements_id = [e.id for e in actual_elements]
+            for i in map(lambda e: e['id'], data['elements']):
+                self.assertIn(i, actual_elements_id)
+
+        # test all
+        test()
+
+        # test none
+        test([self.num_elements + 1])
+
+        # test with some
+        test([t.id for t in self.tasks[:4]])
+
+    def test_filter_categories_ok(self):
+        def test(categories: List[int] = None):
+            actual_elements = self.elements
+
+            kwargs = {}
+            if categories is not None:
+                kwargs['category'] = categories
+                actual_elements = list(filter(lambda e: e.task.category_id in categories, self.elements))
+
+            elements, _ = HistoryElementMixin.query_elements(**kwargs)
+            elements = elements.all()
+
+            self.assertEqual(len(elements), len(actual_elements))
+            for element in actual_elements:
+                self.assertIn(element, elements)
+
+            url = flask.url_for('test')
+            if categories is not None and len(categories) > 0:
+                url += '?category={}'.format('&category='.join(str(t) for t in categories))
+
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            data = response.get_json()
+
+            actual_elements_id = [e.id for e in actual_elements]
+            for i in map(lambda e: e['id'], data['elements']):
+                self.assertIn(i, actual_elements_id)
+
+        # test all
+        test()
+
+        # test none
+        test([self.num_categories + 1])
+
+        # test with some
+        test([t.id for t in self.categories[:2]])
+
+    def test_filter_timeflip(self):
+        def test(timeflips: List[int] = None):
+            actual_elements = self.elements
+
+            kwargs = {}
+            if timeflips is not None:
+                kwargs['category'] = timeflips
+                if self.device.id not in timeflips:
+                    actual_elements = []
+
+            elements, _ = HistoryElementMixin.query_elements(**kwargs)
+            elements = elements.all()
+
+            self.assertEqual(len(elements), len(actual_elements))
+            for element in actual_elements:
+                self.assertIn(element, elements)
+
+            url = flask.url_for('test')
+            if timeflips is not None and len(timeflips) > 0:
+                url += '?timeflip={}'.format('&timeflip='.join(str(t) for t in timeflips))
+
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            data = response.get_json()
+
+            actual_elements_id = [e.id for e in actual_elements]
+            for i in map(lambda e: e['id'], data['elements']):
+                self.assertIn(i, actual_elements_id)
+
+        # test all
+        test()
+
+        # test none
+        test([self.device.id + 5])
+
+    def test_negative_task_ko(self):
+        response = self.client.get(flask.url_for('test') + '?task=-1')
+        self.assertEqual(response.status_code, 422)
+
+    def test_negative_category_ko(self):
+        response = self.client.get(flask.url_for('test') + '?category=-1')
+        self.assertEqual(response.status_code, 422)
+
+    def test_negative_timeflip_ko(self):
+        response = self.client.get(flask.url_for('test') + '?timeflip=-1')
+        self.assertEqual(response.status_code, 422)
 
 
 class HistoryTestCase(FlaskTestCase):
@@ -135,20 +390,6 @@ class HistoryTestCase(FlaskTestCase):
         # test outside bond (get 404)
         self.assertEqual(len(elmts(self.num_elements + 1, 1, expected_status=404)), 0)
         self.assertEqual(len(elmts(-2, 1, expected_status=422)), 0)
-
-    def test_get_history_elements_timeflip_ok(self):
-        self.assertEqual(self.num_elements, HistoryElement.query.count())
-
-        response = self.client.get(flask.url_for('api.history-els') + '?timeflip={}'.format(self.device.id))
-        self.assertEqual(response.status_code, 200)
-        data = response.get_json()
-        self.assertEqual(self.num_elements, data['total_elements'])
-
-        response = self.client.get(
-            flask.url_for('api.history-els') + '?timeflip={}'.format(TimeFlipDevice.query.count() + 1))
-        self.assertEqual(response.status_code, 200)
-        data = response.get_json()
-        self.assertEqual(0, data['total_elements'])
 
     def test_get_history_element_ok(self):
         self.assertEqual(self.num_elements, HistoryElement.query.count())
